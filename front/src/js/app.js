dc2678639522f32f86d9aad9c66705ca57a9389c
Andre Arkhipov <andre.arhipow@gmail.com>
2023-05-31 10:25:42 +0300

    commit

--- front/src/js/app.js
+++ front/src/js/app.js
@@ -1,7 +1,7 @@
 import Modal from './Chat/Modal';
 
 /* eslint-disable */
-console.log('it works!');
+// console.log('it works!');
 
 const modal = new Modal(document.querySelector('.chat-container'));
 
